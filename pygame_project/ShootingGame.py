24eed8e8d088e8ee86bddbbd96e15ef120899ce5
jithub91516 <cyb91516@gmail.com>
2021-02-02 00:56:08 +0900

    third still has a bug

--- pygame_project/ShootingGame.py
+++ pygame_project/ShootingGame.py
@@ -40,7 +40,19 @@ character_height = character_size[1]
 character_x_pos = (screen_width/2) - (character_width/2)
 character_y_pos = screen_height - character_height - stage_height
 
+#character direction
+character_to_x = 0
 
+#character speed
+character_speed = 5
+
+#weapon
+weapon = pygame.image.load(os.path.join(image_path,"weapon.png"))
+weapon_size = weapon.get_rect().size
+weapon_width = weapon_size[0]
+#weapon speed
+weapons = []
+weapon_speed = 10
 
 running = True
 while running: 
@@ -51,16 +63,46 @@ while running:
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT: 
             running = False 
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:# toTheLeft
+                character_to_x -= character_speed
+            elif event.key == pygame.K_RIGHT: #toTheRight
+                character_to_x += character_speed
+            elif event.key == pygame.K_SPACE: #weapon 
+                weapon_x_pos = character_x_pos + (character_width/2) - (weapon_width/2)
+                weapon_y_pos = character_y_pos
+                weapon.append([weapon_x_pos, weapon_y_pos])
+
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                character_to_x = 0
+        
     
     # 3. positions of characters 
+    character_x_pos += character_to_x
+
+    if character_x_pos < 0:
+        character_x_pos = 0
+    elif character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
+
+    #position of the weapons
+    weapons = [[w[0], w[1] - weapon_speed] for w in weapons]
 
     # 4. check up the collision
 
     # 5. draw on the screen
     screen.blit(background, (0, 0))
+    
+    for weapon_x_pos, weapon_y_pos in weapons:
+        screen.blit(weapon, (weapon_x_pos,weapon_y_pos))
+
     screen.blit(stage, (0, screen_height - stage_height))
     screen.blit(character, (character_x_pos, character_y_pos))
 
+    
     pygame.display.update()    
 
 #pygame exit
